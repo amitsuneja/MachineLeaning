04b43acea9c7da56b53094ba526723ccf6b63dba
amits <amitsuneja.devops@gmail.com>
2019-12-20 10:50:42 +0530

    modified:   AndrewNG/Week3/Excercise04/Excercise4.py

--- AndrewNG/Week3/Excercise04/Excercise4.py
+++ AndrewNG/Week3/Excercise04/Excercise4.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import PolynomialFeatures
 import scipy.optimize as opt    # more on this later
 
 """
+https://towardsdatascience.com/andrew-ngs-machine-learning-course-in-python-regularized-logistic-regression-lasso-regression-721f311130fb
 https://www.kaggle.com/ashishrane7/logistic-regression-non-linear-decision-boundary/notebook
 https://anaconda.org/rafaelhbarros/logistic_regression/notebook
 https://medium.com/analytics-vidhya/python-implementation-of-andrew-ngs-machine-learning-course-part-2-2-dceff1a12a12
@@ -63,25 +64,26 @@ plt.scatter(x1_fail, x2_fail, marker="o", color="red", label="fail")
 plt.legend()
 plt.xlabel("test1")
 plt.ylabel("test2")
-#  plt.show()
+plt.show()
+plt.cla()   # Clear axis
+plt.clf()   # Clear figure
+plt.close() # Close a figure window
+
+
 x = df[df.columns[:-1]].values
 poly = PolynomialFeatures(6)
 x = (poly.fit_transform(x))
 """this has converted (118,2) into (118, 28) . So from 2 col/dimension  to 28 col/dimension"""
-
 y = df.iloc[:, 2]
-print(x.shape)
+# print(x.shape)
 # Set the regularization factor to 1
 reg_factor_Lambda = 1
 (m, n) = x.shape
 y = y[:, np.newaxis]
 theta = np.zeros((n, 1))
 reg_factor_Lambda = 1
-
 Initial_Cost = cost_function(theta, x, y, reg_factor_Lambda)
 print("Initial_Cost =", Initial_Cost)
-
-
 output = opt.fmin_tnc(func=cost_function, x0=theta.flatten(),
                       fprime=first_deri_j, args=(x, y.flatten(), reg_factor_Lambda))
 theta = output[0]
@@ -89,6 +91,25 @@ print(theta)  # theta contains the optimized values
 
 
 # Accuracy of model
-pred = sigmoid(z(x, theta)) >= 0.5
-accuracy = np.mean(pred == y.flatten()) * 100
+prediction = sigmoid(z(x, theta)) >= 0.5
+accuracy = np.mean(prediction == y.flatten()) * 100
 print("accuracy = {:.2f}%".format(accuracy))
+print("theta.shape= ", theta.shape)
+
+# Decision Boundary
+u = np.linspace(-1, 1.5, 50)
+v = np.linspace(-1, 1.5, 50)
+z = np.zeros((len(u), len(v)))
+poly = PolynomialFeatures(6)
+for i in range(len(u)):
+    for j in range(len(v)):
+        my_array = np.array([[u[i]], [v[j]]]).reshape(1, 2)
+        z[i, j] = np.dot(poly.fit_transform(my_array), theta)
+plt.scatter(x1_pass, x2_pass, marker="o", color="green", label="pass")
+plt.scatter(x1_fail, x2_fail, marker="o", color="red", label="fail")
+plt.contour(u, v, z, 0)
+plt.legend()
+plt.xlabel("test1")
+plt.ylabel("test2")
+plt.show()
+
---
+++ AndrewNG/Week3/Excercise04/test.py
@@ -0,0 +1,3 @@
+import numpy as np
+myarray = np.array([42, 43])
+print(myarray)
