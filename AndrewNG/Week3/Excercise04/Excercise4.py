2f23c16d56151bb62ad09db8abaa694930930ef3
amits <amitsuneja.devops@gmail.com>
2019-12-20 08:46:10 +0530

    new file:   AdvancePythonFunctions/Polynomials.py 	deleted:    AndrewNG/Exercise4-LogisticRegressionandNewtonMethod/Excercise4.py 	deleted:    AndrewNG/Exercise4-LogisticRegressionandNewtonMethod/Excercise4_New.py 	deleted:    AndrewNG/Exercise4-LogisticRegressionandNewtonMethod/sample1.csv 	new file:   AndrewNG/Week3/Excercise04/Excercise4.py 	new file:   AndrewNG/Week3/Excercise04/polys.py 	new file:   AndrewNG/Week3/ExcerciseQuestions/.ipynb_checkpoints/Untitled-checkpoint.ipynb 	renamed:    AndrewNG/Week3/Week3_LO_Extra/LogisticRegression/.pytest_cache/v/cache/nodeids -> AndrewNG/Week3/ExcerciseQuestions/.pytest_cache/v/cache/nodeids 	new file:   AndrewNG/Week3/ExcerciseQuestions/LearnLogisticRegressionBinaryClassifier.ipynb 	deleted:    AndrewNG/Week3/Week3_LO_Extra/LogisticRegression/.ipynb_checkpoints/BasicsOfLogisticRegression-checkpoint.ipynb 	deleted:    AndrewNG/Week3/Week3_LO_Extra/LogisticRegression/.pytest_cache/v/cache/lastfailed 	deleted:    AndrewNG/Week3/Week3_LO_Extra/LogisticRegression/BasicsOfLogisticRegression.ipynb 	deleted:    AndrewNG/Week3/Week3_LO_Extra/LogisticRegression/LearnLogisticRegressionBinaryClassifier.py 	deleted:    AndrewNG/Week3/Week3_LO_Extra/LogisticRegression/LogisticRegressionFirstBinaryClassifier-CostFunction.png 	deleted:    AndrewNG/Week3/Week3_LO_Extra/LogisticRegression/LogisticRegressionFirstBinaryClassifier-DecisionBoundry.png 	deleted:    AndrewNG/Week3/Week3_LO_Extra/LogisticRegression/LogisticRegressionFirstBinaryClassifier.py 	deleted:    AndrewNG/Week3/Week3_LO_Extra/LogisticRegression/LogisticRegressionGeeksBinaryClassifier.py 	deleted:    AndrewNG/Week3/Week3_LO_Extra/LogisticRegression/Pages/LogisticRegression01.jpg 	deleted:    AndrewNG/Week3/Week3_LO_Extra/LogisticRegression/sample1.csv 	deleted:    AndrewNG/Week3/Week3_LO_Extra/LogisticRegression/sample2.csv 	deleted:    AndrewNG/Week3/Week3_LO_Extra/LogisticRegression/sample3.csv

---
+++ AndrewNG/Week3/Excercise04/Excercise4.py
@@ -0,0 +1,93 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+import scipy.optimize as opt    # more on this later
+
+"""
+https://www.kaggle.com/ashishrane7/logistic-regression-non-linear-decision-boundary/notebook
+https://anaconda.org/rafaelhbarros/logistic_regression/notebook
+https://medium.com/analytics-vidhya/python-implementation-of-andrew-ngs-machine-learning-course-part-2-2-dceff1a12a12
+"""
+
+"""
+You will implement regularized logistic regression to predict whether microchips from a fabrication 
+plant passes quality assurance (QA). During QA, each microchip goes through various tests to ensure it is 
+functioning correctly.
+Suppose you are the product manager of the factory and you have the test results for some microchips on two 
+different tests. From these two tests, you would like to determine whether the microchips should be accepted 
+or rejected. To help you make the decision, you have a dataset of test results on past microchips, 
+from which you can build a logistic regression model.
+"""
+
+
+def z(x, theta):
+    return np.dot(x, theta)
+
+
+def sigmoid(Z):
+    return 1 / (1 + np.exp(-Z))  # note outer brackets in denominator are important else it bad/wrong result.
+
+
+def cost_function(theta, x, y, reg_factor_Lambda):
+    m = x.shape[0]
+    h = sigmoid(z(x, theta))
+    j = (1 / m) * (np.dot(np.log(h).T, -y) - np.dot(np.log(1 - h).T, (1 - y)))
+
+    # Regularization Term
+    # reg_term = (reg_factor_Lambda * np.dot(theta.T, theta)) / (2 * m)
+    reg_term = (reg_factor_Lambda * sum(theta**2))/(2 * m)
+    # Regularized cost
+    j = j + reg_term
+    return j
+
+
+def first_deri_j(theta, x, y, reg_factor_Lambda):
+    m = x.shape[0]
+    h = sigmoid(z(x, theta))
+    calculation = np.zeros([m, 1])
+    calculation = (1 / m) * np.dot(x.T, (h - y))
+    calculation[1:] = calculation[1:] + (reg_factor_Lambda / m) * theta[1:]
+    return calculation
+
+
+df = pd.read_csv('ex2data2.txt', header=None)
+df.columns = ['x1', 'x2', 'y']
+mask = df["y"] == 1
+x1_pass = df[mask]["x1"]
+x2_pass = df[mask]["x2"]
+x1_fail = df[~mask]["x1"]
+x2_fail = df[~mask]["x2"]
+plt.scatter(x1_pass, x2_pass, marker="o", color="green", label="pass")
+plt.scatter(x1_fail, x2_fail, marker="o", color="red", label="fail")
+plt.legend()
+plt.xlabel("test1")
+plt.ylabel("test2")
+#  plt.show()
+x = df[df.columns[:-1]].values
+poly = PolynomialFeatures(6)
+x = (poly.fit_transform(x))
+y = df.iloc[:,2]
+"""this has converted (118,2) into (118, 28) . So from 2 col/dimension  to 28 col/dimension"""
+print(x.shape)
+# Set the regularization factor to 1
+reg_factor_Lambda = 1
+(m, n) = x.shape
+y = y[:, np.newaxis]
+theta = np.zeros((n, 1))
+reg_factor_Lambda = 1
+
+Initial_Cost = cost_function(theta, x, y, reg_factor_Lambda)
+print("Initial_Cost =", Initial_Cost)
+
+
+output = opt.fmin_tnc(func=cost_function, x0=theta.flatten(),
+                      fprime=first_deri_j, args=(x, y.flatten(), reg_factor_Lambda))
+theta = output[0]
+print(theta)  # theta contains the optimized values
+
+
+# Accuracy of model
+pred = sigmoid(z(x, theta)) >= 0.5
+accuracy = np.mean(pred == y.flatten()) * 100
+print("accuracy = {}%".format(accuracy))
