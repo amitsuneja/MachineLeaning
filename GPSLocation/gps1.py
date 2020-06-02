de38419e786e43562fb96ca2430a014370980ec3
amits <amitsuneja.devops@gmail.com>
2020-06-03 02:48:31 +0530

    FirstCommit

--- GPSLocation/gps1.py
+++ GPSLocation/gps1.py
@@ -1,4 +1,5 @@
 import requests
+import pprint
 
 def display_ip_details():
     """  Function To Print GeoIP Latitude & Longitude """
@@ -6,7 +7,7 @@ def display_ip_details():
     my_ip = ip_request.json()['ip']
     geo_request = requests.get('https://get.geojs.io/v1/ip/geo/' +my_ip + '.json')
     geo_data = geo_request.json()
-    print(geo_data)
+    pprint.pprint(geo_data)
 
 if __name__ == '__main__':
     display_ip_details()
